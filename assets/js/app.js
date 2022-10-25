7583b3ff7fe3d5fe02bf876e2d2617e12965d7ea
alfredo <alfredo.arredondo@hotmail.com>
2022-10-25 15:13:28 -0300

    Desafio_JS_001

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,15 +11,12 @@ const colour = document.querySelector("#colour");
 const total = document.querySelector("#total");
 const cantidad = document.querySelector("#cantidad");
 const color = document.querySelector("#color");
-// const laptop = document.querySelector("#laptop")
 const laptop = document.getElementById('laptop');
 boton.addEventListener("click", () => {
     amount.textContent = cantidad.value;
-    colour.textContent = color.value;
+    // colour.textContent = color.value;
+    colour.style.backgroundColor = color.value;
     total.textContent = price * cantidad.value;
-    // laptop.scr = "laptop_"+color.value+"_frente.webp";
     document.getElementById("laptop").src = "/assets/img/laptop_"+color.value+"_frente.webp";
-    // console.log("http://"+document.domain+"/assets/img/laptop_"+color.value+"_frente.webp");
-
 });
 
