a6ca10a5b393b96e8cb95962463b6e5df066f834
alfredo <alfredo.arredondo@hotmail.com>
2022-10-29 09:49:45 -0300

    2022102901

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,9 @@
-var titulo = document.querySelector("h5")
-titulo.style.color = "#7ba238"
-var titulo = document.querySelector("#price")
-titulo.style.color = "#7ba238"
+var titleLaptop = document.querySelector("h5")
+titleLaptop.style.color = "#7ba238"
+var titlePrice = document.querySelector("#price")
+titlePrice.style.color = "#7ba238"
 
-const price = 989559;
-
+const priceValue = document.querySelector("#priceValue");
 const boton = document.querySelector("#boton");
 const amount = document.querySelector("#amount");
 const colour = document.querySelector("#colour");
@@ -13,10 +12,8 @@ const cantidad = document.querySelector("#cantidad");
 const color = document.querySelector("#color");
 const laptop = document.getElementById('laptop');
 boton.addEventListener("click", () => {
-    amount.textContent = cantidad.value;
-    // colour.textContent = color.value;
     colour.style.backgroundColor = color.value;
-    total.textContent = price * cantidad.value;
+    total.textContent = Number(priceValue.textContent) * Number(cantidad.value);
     document.getElementById("laptop").src = "https://alfredoarredondov.github.io/Desafio_JS_001/assets/img/laptop_"+color.value+"_frente.webp";
 });
 
